6c22e053bf2114209a92172c0973fe14ad9ea1dd
frachop <franck.chopin@gmail.com>
2020-07-24 16:26:32 +0200

    read cmdline arguments

--- app/main.cpp
+++ app/main.cpp
@@ -7,8 +7,27 @@
 //
 
 #include <ezWebSockifyLib/ezWebSockifyLib.hpp>
+#include <iostream>
+#include <limits>
 
-int main(int argc, const char * argv[]) {
-	ezWebSockify::run(4822, "192.168.1.49", 5900);
-	return 1;
+int main(int argc, const char * argv[])
+{
+	if (argc != 4)
+	{
+		std::cerr << "Usage: ezWebSockify <wsPort> <tcpHost> <tcpPort>" << std::endl;
+		return 1;
+	}
+
+	auto const wsPort{ std::atoi(argv[1]) };
+	auto const tcpHost{ argv[2] };
+	auto const tcpPort{ std::atoi(argv[3]) };
+
+	if ((wsPort <= 0) || (tcpPort <= 0) || (wsPort > std::numeric_limits<uint16_t>::max()) || (tcpPort > std::numeric_limits<uint16_t>::max()))
+	{
+		std::cerr << "port out of range" << std::endl;
+		return 1;
+	}
+
+	ezWebSockify::run(wsPort, tcpHost, tcpPort);
+	return 0;
 }
